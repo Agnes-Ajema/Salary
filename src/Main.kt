ab053910614f73c5a0dcae31cbf936c8f15cc0c3
Agnes <agnesajema@gmail.com>
2024-02-27 17:44:49 +0300

    add files

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,12 @@
+import kotlin.time.times
+
 fun main(){
  mySalary(9,2000)
 }
 
 fun mySalary(hours:Int, rate: Int){
-    var hours = 9
-    var rate = 2000
-    var salary = (9*2000)
-    println(salary)
+    var mySalary=(hours * rate)
+    println(mySalary)
+
+
 }
